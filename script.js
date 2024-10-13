858cb86fd25a535ed64a9bb1a076c02a6d753684
Igor Valnev <dx2389@gmail.com>
2024-10-13 12:53:19 +0300

    final

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Happy coding!");
-
-  const bigImg = document.querySelector(".middle_second img");
   const colorInputs = document.querySelectorAll("input[type=radio]");
+  const bigImg = document.querySelector(".middle_second img");
 
   const tl = gsap.timeline();
   tl.from(bigImg, { scale: 0, duration: 2 }).to(bigImg, {
@@ -13,23 +11,20 @@ document.addEventListener("DOMContentLoaded", () => {
     ease: "power1.inOut",
   });
 
-  colorInputs.forEach((item) =>
+  colorInputs.forEach((item) => {
     item.addEventListener("click", () => {
       const tl2 = gsap.timeline();
+
       tl2
         .to(bigImg, { scale: 0, duration: 2 })
         .set(bigImg, {
           attr: { src: `./img/${item.value}.svg` },
         })
         .to(bigImg, { scale: 3, duration: 2 })
-        .to(
-          "html",
-          {
-            "--checked-color": item.getAttribute("data-color"),
-            duration: 2,
-          },
-          "-=100%"
-        );
-    })
-  );
+        .to("html", {
+          "--checked-color": item.getAttribute("data-color"),
+          duration: 2,
+        }, "-=100%");
+    });
+  });
 });
